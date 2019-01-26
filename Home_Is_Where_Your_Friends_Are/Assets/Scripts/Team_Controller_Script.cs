7fdecb97d3e5e1298e88a6367717c24311d76a86
ChristopheSaucier <csaucier27@gmail.com>
2019-01-26 10:16:57 -0800

    Added some characters and some overall controller scripts for the team

---
+++ Home_Is_Where_Your_Friends_Are/Assets/Scripts/Team_Controller_Script.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Team_Controller_Script : MonoBehaviour {
+    public List<GameObject> team;
+    private string current_character_string;
+    private GameObject current_character_obj;
+
+	// Use this for initialization
+	void Start () {
+        current_character_string = "1";
+        current_character_obj = team[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("e"))
+        {
+            avengers_assemble();
+        }
+
+        string input = Input.inputString;
+
+        switch (input)
+        {
+            case "1":
+                Debug.Log("Switched to character 1");
+                current_character_obj = team[0];
+                Change_Character_In_Control(0);
+                current_character_string = "1";
+                break;
+            case "2":
+                Debug.Log("Switched to character 2");
+                current_character_obj = team[1];
+                Change_Character_In_Control(1);
+                current_character_string = "2";
+                break;
+            case "3":
+                Debug.Log("Switched to character 3");
+                current_character_obj = team[2];
+                Change_Character_In_Control(2);
+                current_character_string = "3";
+                break;
+            case "4":
+                Debug.Log("Switched to character 4");
+                current_character_obj = team[3];
+                Change_Character_In_Control(3);
+                current_character_string = "4";
+                break;
+        }
+	}
+
+    private void Change_Character_In_Control(int list_index)
+    {
+        foreach(GameObject character in team)
+        {
+            character.GetComponent<Unit_Controller>().being_controlled = false;
+        }
+
+        team[list_index].GetComponent<Unit_Controller>().being_controlled = true;
+    }
+
+    private void avengers_assemble()
+    {
+        foreach(GameObject character in team)
+        {
+            if(character != current_character_obj)
+            {
+                character.transform.position = current_character_obj.transform.position;
+                character.transform.rotation = current_character_obj.transform.rotation;
+            }
+        }
+    }
+}
---
+++ Home_Is_Where_Your_Friends_Are/Assets/Scripts/Unit_Controller.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit_Controller : MonoBehaviour {
+    public GameObject Planet;
+    private Rigidbody2D rigidB;
+    private bool can_jump;
+    public bool being_controlled = false;
+    public bool facing_right = true;
+
+	// Use this for initialization
+	void Start () {
+        rigidB = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (being_controlled)
+        {
+            TakeInputs();
+        }
+	}
+
+    private void TakeInputs()
+    {
+        if (Input.GetKey("a"))
+        {
+            if (facing_right)
+            {
+                facing_right = false;
+                //flip
+                transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+            }
+            
+            bool will_move = true;
+            Debug.DrawLine(transform.position, transform.position + (transform.right * -0.065f), Color.green);
+            RaycastHit2D[] hit2D_d = Physics2D.RaycastAll(transform.position, transform.right * -1, 0.065f);
+
+            foreach(RaycastHit2D rayHit in hit2D_d)
+            {
+                if (rayHit.collider.gameObject.tag == "Planet")
+                {
+                    will_move = false;
+                    Debug.Log("Got something: Left");
+                }
+            }
+
+            if (will_move)
+            {
+                transform.RotateAround(Planet.transform.position, Vector3.back, -1);
+            }
+            
+        }
+
+        if (Input.GetKey("d"))
+        {
+            if (!facing_right)
+            {
+                facing_right = true;
+                //flip
+                transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+            }
+
+            bool will_move = true;
+            Debug.DrawLine(transform.position, transform.position + (transform.right * 0.065f), Color.red);
+            RaycastHit2D[] hit2D_d = Physics2D.RaycastAll(transform.position, transform.right, 0.065f);
+
+            foreach (RaycastHit2D rayHit in hit2D_d)
+            {
+                if (rayHit.collider.gameObject.tag == "Planet")
+                {
+                    will_move = false;
+                    Debug.Log("Got something: Right");
+                }
+            }
+
+            if (will_move)
+            {
+                transform.RotateAround(Planet.transform.position, Vector3.back, 1);
+            }
+
+        }
+
+        if (Input.GetKeyDown("w"))
+        {
+            if (can_jump)
+            {
+                Debug.DrawLine(transform.position, Planet.transform.position);
+                float local_x = transform.up.x;
+                float local_y = transform.up.y;
+                Vector2 local_up = new Vector2(local_x, local_y);
+                rigidB.AddForce(local_up * 100);
+                can_jump = false;
+            }
+        }
+
+        float p_x = Planet.transform.position.x;
+        float p_y = Planet.transform.position.y;
+        float player_x = transform.position.x;
+        float player_y = transform.position.y;
+
+        float angleBetween = Mathf.Atan2(player_y - p_y, player_x - p_x) * 180 / Mathf.PI;
+        //Debug.Log(angleBetween);
+        Vector3 eul = transform.eulerAngles;
+        Quaternion new_rot = Quaternion.Euler(transform.rotation.x, transform.rotation.y, angleBetween - 90);
+        transform.rotation = new_rot;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Planet")
+        {
+            can_jump = true;
+        }
+    }
+}
