04dfad825fb35115e9f881feab022e4e8b67a5cf
Monkeychipz <34406624+Monkeychipz@users.noreply.github.com>
2019-01-26 15:21:03 -0800

    maps

---
+++ Home_Is_Where_Your_Friends_Are/Assets/Scripts/GameInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameInput : MonoBehaviour {
+
+    private InputField input;
+    public GameObject wall;
+    private void Awake()
+    {
+        input = GameObject.Find("InputField").GetComponent<InputField>();
+    }
+
+    public void GetInput(string password)
+    {
+        Debug.Log("Password: " + password);
+        input.text = "";
+        if(password.ToLower() == "open")
+        {
+            wall.SetActive(false);
+        }
+    }
+}
---
+++ Home_Is_Where_Your_Friends_Are/Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour {
+    public int sceneIndex;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            LoadScene(sceneIndex);
+        }
+    }
+    void LoadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
