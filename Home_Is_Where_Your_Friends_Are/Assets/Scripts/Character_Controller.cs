3f53e69866ab062a544e98aed6f39c74562a8c04
redderpanda <jaredsc@uci.edu>
2019-01-26 00:29:48 -0800

    my stuff for the day

--- Home_Is_Where_Your_Friends_Are/Assets/Scripts/Character_Controller.cs
+++ Home_Is_Where_Your_Friends_Are/Assets/Scripts/Character_Controller.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Character_Controller : MonoBehaviour {
+    public float speed = 5f;
+    public float jumpSpeed = 20f;
+    public float airTime = 3f;
+    public float floatY = 0;
     public GameObject Planet;
     private Rigidbody2D rigidB;
-    private bool can_jump;
+    public bool can_jump;
 
 	// Use this for initialization
 	void Start () {
@@ -14,28 +18,50 @@ public class Character_Controller : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        float p_x = Planet.transform.position.x;
+        float p_y = Planet.transform.position.y;
+        float player_x = transform.position.x;
+        float player_y = transform.position.y;
 
-        if (Input.GetKey("a"))
+        float angleBetween = Mathf.Atan2(player_y - p_y, player_x - p_x) * 180 / Mathf.PI;
+        //Debug.Log(angleBetween);
+        Vector3 eul = transform.eulerAngles;
+        Quaternion new_rot = Quaternion.Euler(transform.rotation.x, transform.rotation.y, angleBetween - 90);
+        transform.rotation = new_rot;
+        if (Input.GetKey(KeyCode.D))
         {
-            transform.RotateAround(Planet.transform.position, Vector3.back, -5);
+            angleBetween -= 2;
+            float sin_val = Mathf.Sin(angleBetween * Mathf.PI / 180);
+            float dist = Vector3.Distance(transform.position, Planet.transform.position);
+            Debug.Log(dist);
+            float y = dist * sin_val;
+            float cos_val = Mathf.Cos(angleBetween * Mathf.PI / 180);
+            float x = dist * cos_val;
+            Vector3 new_pos = new Vector3(x, y, transform.position.z);
+            transform.position = new_pos;
         }
-
-        if (Input.GetKey("d"))
+        if(Input.GetKey(KeyCode.A))
         {
-            transform.RotateAround(Planet.transform.position, Vector3.back, 5);
+            angleBetween += 2;
+            float sin_val = Mathf.Sin(angleBetween * Mathf.PI / 180);
+            float dist = Vector3.Distance(transform.position, Planet.transform.position);
+            Debug.Log(dist);
+            float y = dist * sin_val;
+            float cos_val = Mathf.Cos(angleBetween * Mathf.PI / 180);
+            float x = dist * cos_val;
+            Vector3 new_pos = new Vector3(x, y, transform.position.z);
+            transform.position = new_pos;
         }
-
+        
         if (Input.GetKeyDown("w"))
         {
             if (can_jump)
             {
-                Debug.DrawLine(transform.position, Planet.transform.position);
-                float local_x = transform.up.x;
-                float local_y = transform.up.y;
-                Vector2 local_up = new Vector2(local_x, local_y);
-                rigidB.AddForce(local_up * 100);
+                transform.Translate(Vector3.up * 0.15f ,Space.Self);
+
             }
         }
+
 
 	}
 
---
+++ Home_Is_Where_Your_Friends_Are/Assets/Scripts/Follower.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follower : MonoBehaviour {
+    
+    public Transform leader;
+    public float followSharpness = 0.05f;
+    public bool isJumping = false;
+
+    void LateUpdate()
+    {
+        float p_x = leader.GetComponent<Character_Controller>().Planet.transform.position.x;
+        float p_y = leader.GetComponent<Character_Controller>().Planet.transform.position.y;
+        float player_x = transform.position.x;
+        float player_y = transform.position.y;
+
+        float angleBetween = Mathf.Atan2(player_y - p_y, player_x - p_x) * 180 / Mathf.PI;
+        //Debug.Log(angleBetween);
+        Vector3 eul = transform.eulerAngles;
+        Quaternion new_rot = Quaternion.Euler(transform.rotation.x, transform.rotation.y, angleBetween - 90);
+        transform.rotation = new_rot;
+        transform.position += (leader.position - transform.position) * followSharpness;
+        if(!leader.GetComponent<Character_Controller>().can_jump && !isJumping)
+        {
+            transform.Translate(Vector3.up * 0.04f, Space.Self);
+        }
+    }
+}
