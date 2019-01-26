09d1a226b9305445942f6d09d352e50b80a3fa10
ChristopheSaucier <csaucier27@gmail.com>
2019-01-26 10:31:53 -0800

    Merged Unit_Control scripts into one ultimate script

--- Home_Is_Where_Your_Friends_Are/Assets/Scripts/Unit_Controller.cs
+++ Home_Is_Where_Your_Friends_Are/Assets/Scripts/Unit_Controller.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Unit_Controller : MonoBehaviour {
+    public float speed = 5f;
+    public float jumpSpeed = 20f;
+    public float airTime = 3f;
+    public float floatY = 0;
     public GameObject Planet;
     private Rigidbody2D rigidB;
     private bool can_jump;
@@ -18,9 +22,102 @@ public class Unit_Controller : MonoBehaviour {
 	void Update () {
         if (being_controlled)
         {
-            TakeInputs();
+            Jared_Take_Inputs();
         }
 	}
+
+    private void Jared_Take_Inputs()
+    {
+        float p_x = Planet.transform.position.x;
+        float p_y = Planet.transform.position.y;
+        float player_x = transform.position.x;
+        float player_y = transform.position.y;
+
+        float angleBetween = Mathf.Atan2(player_y - p_y, player_x - p_x) * 180 / Mathf.PI;
+        //Debug.Log(angleBetween);
+        Vector3 eul = transform.eulerAngles;
+        Quaternion new_rot = Quaternion.Euler(transform.rotation.x, transform.rotation.y, angleBetween - 90);
+        transform.rotation = new_rot;
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (!facing_right)
+            {
+                facing_right = true;
+                //flip
+                transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+            }
+
+            bool will_move = true;
+            Debug.DrawLine(transform.position, transform.position + (transform.right * 0.065f), Color.red);
+            RaycastHit2D[] hit2D_d = Physics2D.RaycastAll(transform.position, transform.right, 0.065f);
+
+            foreach (RaycastHit2D rayHit in hit2D_d)
+            {
+                if (rayHit.collider.gameObject.tag == "Planet")
+                {
+                    will_move = false;
+                    Debug.Log("Got something: Right");
+                }
+            }
+
+            if (will_move)
+            {
+                angleBetween -= 2;
+                float sin_val = Mathf.Sin(angleBetween * Mathf.PI / 180);
+                float dist = Vector3.Distance(transform.position, Planet.transform.position);
+                Debug.Log(dist);
+                float y = dist * sin_val;
+                float cos_val = Mathf.Cos(angleBetween * Mathf.PI / 180);
+                float x = dist * cos_val;
+                Vector3 new_pos = new Vector3(x, y, transform.position.z);
+                transform.position = new_pos;
+            }
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (facing_right)
+            {
+                facing_right = false;
+                //flip
+                transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+            }
+
+            bool will_move = true;
+            Debug.DrawLine(transform.position, transform.position + (transform.right * -0.065f), Color.green);
+            RaycastHit2D[] hit2D_d = Physics2D.RaycastAll(transform.position, transform.right * -1, 0.065f);
+
+            foreach (RaycastHit2D rayHit in hit2D_d)
+            {
+                if (rayHit.collider.gameObject.tag == "Planet")
+                {
+                    will_move = false;
+                    Debug.Log("Got something: Left");
+                }
+            }
+
+            if (will_move)
+            {
+                angleBetween += 2;
+                float sin_val = Mathf.Sin(angleBetween * Mathf.PI / 180);
+                float dist = Vector3.Distance(transform.position, Planet.transform.position);
+                Debug.Log(dist);
+                float y = dist * sin_val;
+                float cos_val = Mathf.Cos(angleBetween * Mathf.PI / 180);
+                float x = dist * cos_val;
+                Vector3 new_pos = new Vector3(x, y, transform.position.z);
+                transform.position = new_pos;
+            }
+        }
+
+        if (Input.GetKeyDown("w"))
+        {
+            if (can_jump)
+            {
+                transform.Translate(Vector3.up * 0.15f, Space.Self);
+
+            }
+        }
+    }
 
     private void TakeInputs()
     {
